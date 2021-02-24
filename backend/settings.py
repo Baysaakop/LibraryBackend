d38f92871ed5d49d0dfdbca27326927d6a1c29e3
Baysaakop <baysaakop@gmail.com>
2021-02-24 14:47:26 +0800

    4th commit

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'c^-*o(i9ky+-^+=3ll1%%xang(f#c(!%$ummqykw)8v#&iws!)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'otlibraryback.herokuapp.com']
 
 
 # Application definition
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 24,
+    'PAGE_SIZE': 40,
 }
 
 REST_AUTH_SERIALIZERS = {
--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),    
     path('rest-auth/', include('rest_auth.urls')),    
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path("api/items/", include('items.urls')),
